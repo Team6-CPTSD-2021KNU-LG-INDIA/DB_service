83e303493c857b4d633623f9fa698dc9192efcf1
Seenabro97 <61000848+Seenabro97@users.noreply.github.com>
2021-07-27 15:49:58 +0900

    Add files via upload

--- testservice/helloworld_webos_service.js
+++ testservice/helloworld_webos_service.js
@@ -13,135 +13,7 @@ const Service = require('webos-service');
 
 const service = new Service(pkgInfo.name); // Create service by service name on package.json
 const logHeader = "[" + pkgInfo.name + "]";
-let greeting = "Hello, World!";
 const service_name='luna-send -n 1 -f -m com.palm.configurator luna://com.webos.service.db/';
-
-// a method that always returns the same value
-service.register("hello", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/hello");
-    console.log("In hello callback");
-    const name = message.payload.name ? message.payload.name : "World";
-
-    message.respond({
-        returnValue: true,
-        Response: "Hello, " + name + "!"
-    });
-});
-
-// set some state in the service
-service.register("/config/setGreeting", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/config/setGreeting");
-    console.log("In setGreeting callback");
-    if (message.payload.greeting) {
-        greeting = message.payload.greeting;
-    } else {
-        message.respond({
-            returnValue: false,
-            errorText: "argument 'greeting' is required",
-            errorCode: 1
-        });
-    }
-    message.respond({
-        returnValue: true,
-        greeting: greeting
-    });
-});
-
-// call another service
-service.register("time", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/time");
-    console.log("time callback");
-    service.call("luna://com.webos.service.systemservice/clock/getTime", {}, function(m2) {
-        console.log(logHeader, "SERVICE_METHOD_CALLED:com.webos.service.systemservice/clock/getTime");
-        const response = "You appear to have your UTC set to: " + m2.payload.utc;
-        console.log(response);
-        message.respond({message: response});
-    });
-});
-
-// handle subscription requests
-const subscriptions = {};
-let interval;
-let x = 1;
-function createInterval() {
-    if (interval) {
-        return;
-    }
-    console.log(logHeader, "create_interval");
-    console.log("create new interval");
-    interval = setInterval(function() {
-        sendResponses();
-    }, 1000);
-}
-
-// send responses to each subscribed client
-function sendResponses() {
-    console.log(logHeader, "send_response");
-    console.log("Sending responses, subscription count=" + Object.keys(subscriptions).length);
-    for (const i in subscriptions) {
-        if (Object.prototype.hasOwnProperty.call(subscriptions, i)) {
-            const s = subscriptions[i];
-            s.respond({
-                returnValue: true,
-                event: "beat " + x
-            });
-        }
-    }
-    x++;
-}
-
-// listen for requests, and handle subscriptions via implicit event handlers in call
-// to register
-service.register("heartbeat", function(message) {
-    const uniqueToken = message.uniqueToken;
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat");
-    console.log("heartbeat callback, uniqueToken: " + uniqueToken + ", token: " + message.token);
-    message.respond({event: "beat"});
-    if (message.isSubscription) {
-        subscriptions[uniqueToken] = message;
-        if (!interval) {
-            createInterval();
-        }
-    }
-},
-function(message) {
-    const uniqueToken = message.uniqueToken;
-    console.log("Canceled " + uniqueToken);
-    delete subscriptions[uniqueToken];
-    const keys = Object.keys(subscriptions);
-    if (keys.length === 0) {
-        console.log("no more subscriptions, canceling interval");
-        clearInterval(interval);
-        interval = undefined;
-    }
-});
-
-// EventEmitter-based API for subscriptions
-// note that the previous examples are actually using this API as well, they're
-// just setting a "request" handler implicitly
-const heartbeat2 = service.register("heartbeat2");
-heartbeat2.on("request", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat2/request");
-    console.log("heartbeat callback");
-    message.respond({event: "beat"});
-    if (message.isSubscription) {
-        subscriptions[message.uniqueToken] = message;
-        if (!interval) {
-            createInterval();
-        }
-    }
-});
-heartbeat2.on("cancel", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat2/cancel");
-    console.log("Canceled " + message.uniqueToken);
-    delete subscriptions[message.uniqueToken];
-    const keys = Object.keys(subscriptions);
-    if (keys.length === 0) {
-        console.log("no more subscriptions, canceling interval");
-        clearInterval(interval);
-        interval = undefined;
-    }
-});
 
 service.register("ping", function(message) {
     console.log(logHeader, "SERVICE_METHOD_CALLED:/ping");
@@ -204,6 +76,8 @@ service.register("createDB",function(message){
             {"name":"cnt","props":[{"name":"content"}]}
         ]
     };
+    //message.payload.returnValue
+    //message.payload.results[0].payload._id
     const permission={ 
         "permissions":[ 
            { 
@@ -226,12 +100,12 @@ service.register("createDB",function(message){
         }
         else{
             service.call(service_name+'putKind',kind,function(reply){
-                message.respond({msg:"create kind"})
+                message.respond({msg:"create kind"});
+                service.call(service_name+'putPermission',permission);
             })
         }
     })
 
-    service.call(service_name+'putPermission',permission)
 });
 
 service.register("enrollEvent",function(message){
@@ -246,45 +120,33 @@ service.register("enrollEvent",function(message){
              "repeat":message.payload.repeat,
              "content":message.payload.content
         }
-    ]}
+    ]};
 
     service.call(service_name+'put',kind_object);
 });
 
-service.register("ListEvent",function(message){
+service.register("modifyEvent",function(message){
+    const id=message.payload.id;
     const start_time=new Date(message.payload.startTime);
     const end_time=new Date(message.payload.endTime);
-    const query={
-        "query": {
-            "from":"com.webos.service.event:1",
-            "where":[
-                {"prop":"startTime",
-                "op":">=",
-                "val":start_time}
-            ],
-            "where":[
-                {"prop":"endTime",
-                "op":"<=",
-                "val":end_time}
-            ]
+    const kind_object={
+        "objects":[ 
+          { 
+             "id":id, 
+             "_kind":"com.webos.service.event:1",
+             "startTime":start_time,
+             "endTime":end_time,
+             "repeat":message.payload.repeat,
+             "content":message.payload.content
         }
-    };
-    service.call(service_name+'find',query);
+    ]}
+
+    service.call(service_name+'put',kind_object);
+});
+service.register("deleteEvent",function(message){
+    const id=messge.payload.id;
 });
 
-// luna-send -n 1 -f -m com.palm.configurator luna://com.webos.service.db/find '{"query": {"from":"com.webos.service.employees:3"}}'
-service.register("pong", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/pong");
-    console.log("Pong!");
-    console.log(message.payload);
-    message.respond({message: "Pong"});
-});
-
-service.register("/do/re/me", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED://do/re/me");
-    message.respond({verses:[
-        {doe: "a deer, a female deer"},
-        {ray: "a drop of golden sun"},
-        {me: "a name I call myself"}
-    ]});
-});
+// service.register("ListEvent",function(message){
+//     service.call(service_name+'find',{},);
+// });
